db4ebfe6b5c39da3cb21e93557970c259f42f761
Armando Roberto Travieso Montes <armandortmontes@gmail.com>
2025-08-30 16:16:51 -0400

    feat: using sweet alert 2.

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Router } from '@angular/router';
-import { Component, inject } from '@angular/core';
+import { Component, inject, Inject, PLATFORM_ID, OnInit } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-home',
@@ -7,12 +8,32 @@ import { Component, inject } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   router = inject(Router);
 
-  constructor() {}
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+
+  ngOnInit(): void {
+    
+    if (isPlatformBrowser(this.platformId)) {
+      this.showAlert();
+    }
+  }
 
   redirectToHome(): void {
     this.router.navigateByUrl('/');
+  }
+
+  async showAlert(): Promise<void> {
+    // Dynamic import to avoid SSR issues
+    const Swal = await import('sweetalert2');
+
+    Swal.default.fire({
+      position: 'top-end',
+      icon: 'success',
+      title: 'User authenticated successfully.',
+      showConfirmButton: false,
+      timer: 1500,
+    });
   }
 }
