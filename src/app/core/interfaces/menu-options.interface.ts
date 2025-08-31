56757a55580b8e5d16579c093aca835c0fa8398f
Armando Roberto Travieso Montes <armandortmontes@gmail.com>
2025-08-31 10:47:48 -0400

    feat: adding menu options icons and strucutre.

---
+++ src/app/core/interfaces/menu-options.interface.ts
@@ -0,0 +1,5 @@
+export interface IMenuOptions {
+  id: number;
+  name: string;
+  image: string;
+}
--- src/app/views/home/header/header.component.ts
+++ src/app/views/home/header/header.component.ts
@@ -1,3 +1,4 @@
+import { IMenuOptions } from '@core/interfaces/menu-options.interface';
 import { Component, inject, signal } from '@angular/core';
 import { CommonModule } from "@angular/common";
 import { Router } from '@angular/router';
@@ -11,6 +12,28 @@ import { Router } from '@angular/router';
 export class HeaderComponent {
   isMenuOpened = signal<boolean>(false);
   router = inject(Router);
+  menuOptions: IMenuOptions[] = [
+    {
+      id: 1,
+      name: 'Home',
+      image: '/assets/images/home.png'
+    },
+    {
+      id: 2,
+      name: 'Streams',
+      image: '/assets/images/stream.png'
+    },
+    {
+      id: 3,
+      name: 'Party',
+      image: '/assets/images/party.png'
+    },
+    {
+      id: 4,
+      name: 'Premium',
+      image: '/assets/images/premium.png'
+    },
+  ]
 
   openMenu(): void {
     this.isMenuOpened.set(!this.isMenuOpened());
